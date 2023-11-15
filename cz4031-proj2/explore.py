6b023dce19dd7812799da176abcc997c171f631f
hashtagyx <c.yongxuan@gmail.com>
2023-11-15 22:53:10 +0800

    Cleaned up and annotated explore.py and project.py

--- cz4031-proj2/explore.py
+++ cz4031-proj2/explore.py
@@ -1,14 +1,15 @@
+# Import necessary modules
 import psycopg2
 import sqlglot.expressions as exp
 from sqlglot import parse_one, exp
 import jsonify
 import ast
-import json
 import re
 
+# Connects to the local PostgreSQL database with the provided parameters
 def connect_to_database(params):
     try:
-        # Connect to your postgres DB
+        # Connect to your PostgreSQL DB
         conn = psycopg2.connect(
             dbname=params["dbname"],
             user=params["user"],
@@ -20,110 +21,175 @@ def connect_to_database(params):
         # Handle connection error
         raise psycopg2.OperationalError(f"Database connection failed: {e}")
 
+# Executes a given SQL query using the provided connection parameters
+# Returns a dictionary/JSON object:
+# {
+#     'explain_result': JSON array containing the output from an EXPLAIN query
+#     'block_dict': JSON object containing the blocks accessed by each table in the query. Keys are the table name and values are the list of block numbers accessed.
+#     'block_result': JSON object containing the tuples of each table in the query. Keys are the table names, values are the list of blocks of each table. Within each list of blocks are the individual tuple entries.
+# }
 def execute_query(query, connection_params):
-    # Remove all trailing spaces and convert to lowercase
+    # Preprocessing the query: lowercasing, stripping spaces, and removing semicolons
     query = query.lower().strip()
-    # Remove all trailing semicolons
     query = query.replace(";", "")
+
+    # Execute the query if it's not empty
     if query:
+        # Run the EXPLAIN query to get the execution plan
         explain_result = run_explain_query(query, connection_params)
 
-        if explain_result == []:
+        # If there's no result from the EXPLAIN query, return empty data (botched query submitted)
+        if not explain_result:
             return {
                 'explain_result': [],
                 'block_dict': {},
                 'block_result': {}
             }
 
+        # Run query to get the CTID values (tuple identifiers in PostgreSQL)
         ctid_result = run_ctid_query(query, connection_params)
 
-        # get fetched blocks and tuples
+        # If there's no result from the CTID query, return empty data (botched query submitted)
+        if not ctid_result:
+            return {
+                'explain_result': [],
+                'block_dict': {},
+                'block_result': {}
+            }
+
+        # Obtain the fetched blocks and tuples from the original query
         block_dict, tuple_dict = get_fetched_blocks_and_tuples(query, ctid_result)
 
+        # Run a query to get all CTID values of blocks
         all_result = all_ctid_query(query, tuple_dict, connection_params)
-        block_result = {}
-    
-        for table_name in all_result:
-            block_result[table_name] = []
-            column_name = table_name + "_ctid"
-            last_block_no = ast.literal_eval(all_result[table_name][0][column_name])[0]
-            last_block = []
-            for row in all_result[table_name]:
-                # converts the string to a tuple
-                ctid = ast.literal_eval(row[column_name])
-                block_no = ctid[0]
-                if last_block_no != block_no:
-                    block_result[table_name].append(last_block)
-                    last_block = []
-                    last_block_no = block_no
-                if ctid in tuple_dict[table_name]:
-                    row['fetched'] = True
-                else:
-                    row['fetched'] = False
-                last_block.append(row)
-            block_result[table_name].append(last_block)
+
+        # Process all_result with tuple_dict to get block_result. block_result[table_name] is a list of blocks of the table table_name.
+        # block_result[table_name][i] is a list of rows/tuples in the ith block.
+        # block_result[table_name][i][j] is the jth tuple in the ith block.
+        block_result = get_block_result(tuple_dict, all_result)
 
         res =  {
-            'explain_result': explain_result, # gives us the explain result needed for QEP tree generation
-            'block_dict': block_dict, # gives us the blocks that should be highlighted
-            'block_result': block_result # gives us all the tuples of all tables queried in a block-based format
+            'explain_result': explain_result, # Gives us the EXPLAIN result needed for QEP tree generation
+            'block_dict': block_dict, # Gives us the block numbers that should be highlighted
+            'block_result': block_result # Gives us all the tuples of all tables queried in a block-based format
         }
-        testoutput = {
-            'block_dict': block_dict, # gives us the blocks that should be highlighted
-            'block_result': block_result # gives us all the tuples of all tables queried in a block-based format
-        }
-        file_path = 'output.json' # Replace with your file path
-        with open(file_path, 'w') as file:
-            json.dump(testoutput, file)
+
         return res
-    # this line will never run because query input is required in interface.py
+    
+    # This line will never run because query input is required in interface.py
     return jsonify({'error': 'No query provided'}), 400
 
+# Process all_result with tuple_dict to get block_result
+def get_block_result(tuple_dict, all_result):
+    block_result = {}
+    
+    for table_name in all_result:
+         # Initialize an empty list for each table in the block_result dictionary to represent a list of blocks
+        block_result[table_name] = []
+        # Construct the column name for CTID
+        column_name = table_name + "_ctid"
+
+        # Get the block number of the first tuple in the first block of the current table
+        last_block_no = ast.literal_eval(all_result[table_name][0][column_name])[0]
+        # Initialize an empty list to store tuples belonging to the same block
+        last_block = []
+
+        for row in all_result[table_name]:
+            # Convert the CTID string in the row to a tuple (block number, tuple number)
+            ctid = ast.literal_eval(row[column_name])
+            block_no = ctid[0]
+
+            # If the block number changes, append the last block to the result and start a new block
+            if last_block_no != block_no:
+                block_result[table_name].append(last_block)
+                last_block = []
+                last_block_no = block_no
+            
+            # Check if the current tuple's CTID is in the set of fetched tuples
+            if ctid in tuple_dict[table_name]:
+                row['fetched'] = True # Mark the tuple as fetched
+            else:
+                row['fetched'] = False # Mark the tuple as not fetched
+            
+            # Add the current row to the last block
+            last_block.append(row)
+        
+        # Append the last block to the block_result after finishing the loop
+        block_result[table_name].append(last_block)
+    return block_result
+
+# Obtain the blocks and tuples that have been fetched from disk given the query and CTID query result
 def get_fetched_blocks_and_tuples(query, ctid_result):
     table_names = []
+    # Parse the query to find all the table names
     for table in parse_one(query).find_all(exp.Table):
         table_names.append(table.name)
-    # this stores the blocks and tuples that have been fetched (block_dict[table_name] and tuple_dict[table_name])
+
+    # Initialize dictionaries to store the blocks and tuples that have been fetched
     block_dict = {}
     tuple_dict = {}
     for table_name in table_names:
-        block_dict[table_name] = set()
-        tuple_dict[table_name] = set()
+        block_dict[table_name] = set() # Set to store unique block numbers
+        tuple_dict[table_name] = set() # Set to store unique CTIDs
     
+    # Iterate through each row in the CTID result
     for row in ctid_result:
+        # Process each table's fetched blocks and tuples
         for table_name in table_names:
-            column_name = table_name + "_ctid"
-            # converts the string to a tuple
+            column_name = table_name + "_ctid" # Construct the CTID column name for the table
+
+            # Converts the CTID string in the row to a tuple (block number, tuple number)
             ctid = ast.literal_eval(row[column_name])
             block_no = ctid[0]
+
+            # Add the block number and CTID to their respective sets in the dictionaries
             block_dict[table_name].add(block_no)
             tuple_dict[table_name].add(ctid)
     
+    # Convert the sets in block_dict and tuple_dict to sorted lists (so that we can return it as a valid JSON object)
     for table_name in block_dict:
         block_dict[table_name] = sorted(list(block_dict[table_name]))
         tuple_dict[table_name] = sorted(list(tuple_dict[table_name]))
     
     return block_dict, tuple_dict
 
+# Runs the EXPLAIN query in PostgreSQL and returns the result
 def run_explain_query(query, connection_params):
     try:
+        # Connect to the PostgreSQL database using the provided connection parameters
         conn = psycopg2.connect(**connection_params)
         cur = conn.cursor()
+
+        # Generate the EXPLAIN query by prepending the original query
         explain_query = f"EXPLAIN (FORMAT JSON, ANALYZE, BUFFERS, COSTS OFF) {query}"
+
+        # Execute the EXPLAIN query
         cur.execute(explain_query)
+
+        # Fetch the first row of the result set (the query plan in JSON format)
         result = cur.fetchone()
+
+        # Close the cursor and the connection
         cur.close()
         conn.close()
+
+        # Return the query plan (first element of the result tuple)
         return result[0]
+    # Handle exceptions during query execution or connection issues
     except (Exception, psycopg2.Error) as error:
+        # Print the error and return an empty list as the result
         print(f"Error in run_explain_query: {error}")
         return []
 
+# Modifies SQL query by adding CTID selection for each table. Note: 10000 tuple limit for performance.
 def modify_query_add_ctid(sql, table_names):
+    # Extract the part of the SQL query after 'SELECT' (ignoring case)
     rest_of_query = sql[sql.lower().find('select') + len('select'):]
+
+    # Start constructing a new SELECT clause
     new_select_clause = "select "
 
-    # For each table, add ctid as a selected column
+    # For each table, add CTID as a selected column, with the name <table_name>_ctid
     for name in table_names:
         new_select_clause += f"{name}.ctid as {name}_ctid, "
 
@@ -140,6 +206,8 @@ def modify_query_add_ctid(sql, table_names):
 
     return new_sql
 
+# Construct a SQL query that retrieves CTID values and all other columns for a specific table subject to an offset
+# This fetches the first 10000 tuples in ascending CTID values from the specified table following the offset such that the first tuple returned is in the first block hit
 def get_query_all_ctid(table_name, offset_val):
     new_select_clause = f"""select {table_name}.ctid as {table_name}_ctid, * from {table_name}
      order by {table_name}.ctid limit 10000 offset {offset_val}
@@ -147,27 +215,29 @@ def get_query_all_ctid(table_name, offset_val):
 
     return new_select_clause
 
+# Execute queries for CTID values for all tables in the original query, starting from the first block hit
+# Note the LIMIT 10000 in get_query_all_ctid which limits the number of tuples returned for performance purposes
 def all_ctid_query(query, tuple_dict, connection_params):
     try:
+        # Establish a connection to the database
         conn = psycopg2.connect(**connection_params)
         cur = conn.cursor()
+
+        # Extract table names from the given query
         table_names = []
         for table in parse_one(query).find_all(exp.Table):
             table_names.append(table.name)
         
         results = {}
+
+        # Process each table involved in the query
         for table_name in table_names:
+            # Get the CTID of the first block hit in the original query from tuple_dict
             start_block_ctid = tuple_dict[table_name][0]
-            
-            # offset_val_query = f"""
-            # SELECT rownum FROM (
-            #     SELECT ROW_NUMBER() OVER (ORDER BY ctid) AS rownum, ctid
-            #     FROM {table_name}
-            # ) AS subquery
-            # WHERE ctid = '{str(start_block_ctid)}';
-            # """
-          
+            # Extract the block number of the first block hit in the original query
             start_block_number = start_block_ctid[0]
+
+            # Construct a query to find the row number of the first tuple in the first block hit in the original query
             offset_val_query = f"""
             SELECT rownum FROM (
                 SELECT ROW_NUMBER() OVER (ORDER BY ctid) AS rownum, ctid
@@ -178,51 +248,69 @@ def all_ctid_query(query, tuple_dict, connection_params):
             LIMIT 1
             """
 
-            # print("Start Block Number", start_block_number)
-            # print("offset_val_query", offset_val_query)
-            
+            # Execute the query to find the offset value
             cur.execute(offset_val_query)
-            # the query returns the row number of the first tuple found. hence, we subtract one from the
+            # The query returns the row number of the first tuple found. Hence, we subtract one from the
             # query value to obtain the actual offset value
             offset_val = cur.fetchone()[0] - 1
             
+            # Construct the final query using the offset value to find all* the tuples in the table (*subject to LIMIT 10000 and OFFSET to the first block hit)
             all_query = f"""
             SELECT array_to_json(array_agg(row_to_json(t))) FROM (
                 {get_query_all_ctid(table_name, offset_val)}
             ) t
             """
+            # Execute the final query to get the CTIDs and their corresponding row data
             cur.execute(all_query)
             result = cur.fetchone()
+            # Store the result in the results dictionary
             results[table_name] = result[0]
+
         cur.close()
         conn.close()
         return results
+    # Handle exceptions that may occur during database operations
     except (Exception, psycopg2.Error) as error:
+        # Print the error message and return and empty dictionary
         print(f"Error in all_ctid_query: {error}")
-        return []
+        return {}
 
+# Execute a modified version of the input query that includes CTID selection
 def run_ctid_query(query, connection_params):
     try:
+        # Establish a connection to the PostgreSQL database using the provided connection parameters
         conn = psycopg2.connect(**connection_params)
         cur = conn.cursor()
+
+        # Extract table names from the given query
         table_names = []
         for table in parse_one(query).find_all(exp.Table):
             table_names.append(table.name)
+
+        # Modify the input query to include CTID selection for each table
+        # The modified query selects CTIDs along with all other columns
         ctid_query = f"""
         SELECT array_to_json(array_agg(row_to_json(t))) FROM (
             {modify_query_add_ctid(query, table_names)}
         ) t
         """
-        # print("CTID QUERY:", ctid_query)
+        # Execute the modified query
         cur.execute(ctid_query)
+
+        # Fetch the first row from the query result
         result = cur.fetchone()
         cur.close()
         conn.close()
+
+        # Return the result of the query (the first element of the result tuple)
         return result[0]
+    # Handle exceptions that might occur during database operations
     except (Exception, psycopg2.Error) as error:
+        # Print the error message and return an empty list is there is an error
         print(f"Error in run_ctid_query: {error}")
         return []
-    
+
+# Check if input query is valid (i.e. no forbidden keyword, non empty)
 def is_query_valid(query):
     # Check if the query is empty
     if not query.strip():
--- cz4031-proj2/project.py
+++ cz4031-proj2/project.py
@@ -1,20 +1,26 @@
+# Import the App class from the 'interface' module and the 'sys' module
 from interface import App
 import sys
+# Import QApplication from PyQt5, which is needed to create the main application window
 from PyQt5.QtWidgets import QApplication
 
+# Define the database connection parameters as a dictionary
+# Replace the connection parameters with the user's own credentials
 connection_params = {
-    "dbname": "TPC-H",
-    "user": "postgres",
-    "password": "password",
-    "host": "localhost"
+    "dbname": "TPC-H",       # Name of the database
+    "user": "postgres",      # Username to access the database
+    "password": "password",  # Password for the database user
+    "host": "localhost"      # Host address of the database (here, it's the local machine)
 }
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
 
     try:
+        # Create an instance of the 'App' class (the main application window) with the database connection parameters
         ex = App(connection_params=connection_params)
     except ConnectionError as e:
+        # If a ConnectionError occurs (e.g., database connection fails), print the error message and exit the application
         print(e)
         sys.exit(1)
 
